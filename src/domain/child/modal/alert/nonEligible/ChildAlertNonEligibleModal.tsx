b7f5f55b08319ecd434944d5169c52edd39c5593
jps <jpsalo@zoho.com>
2020-03-26 12:45:42 +0200

    Put non eligible child alert into modal

--- src/domain/child/modal/alert/nonEligible/ChildAlertNonEligibleModal.tsx
+++ src/domain/child/modal/alert/nonEligible/ChildAlertNonEligibleModal.tsx
@@ -1,33 +1,30 @@
-import * as React from 'react';
+import React, { FunctionComponent } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import styles from './childAlertNonEligibleModal.module.scss';
 import Icon from '../../../../../common/components/icon/Icon';
 import personIcon from '../../../../../assets/icons/svg/adultFace.svg';
-import Button from '../../../../../common/components/button/Button';
+import AlertModal from '../../../../../common/components/alert/AlertModal';
+
 interface ChildFormModalNonEligibleProps {
   setIsOpen: (value: boolean) => void;
 }
 
-// TODO: Make a modal
-const ChildAlertNonEligibleModal: React.FunctionComponent<ChildFormModalNonEligibleProps> = ({
+const ChildAlertNonEligibleModal: FunctionComponent<ChildFormModalNonEligibleProps> = ({
   setIsOpen,
 }) => {
   const { t } = useTranslation();
+
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.notEligible}>
-        <h3>{t('registration.notEligible.title')}</h3>
-        <p>{t('registration.notEligible.text')}</p>
-        <Icon className={styles.icon} src={personIcon} />
-        <Button
-          className={styles.goBackButton}
-          onClick={() => setIsOpen(false)}
-        >
-          {t('child.form.modal.notEligible.return.text')}
-        </Button>
-      </div>
-    </div>
+    <AlertModal
+      isOpen={true}
+      heading={t('registration.notEligible.title')}
+      ok={t('child.form.modal.notEligible.return.text')}
+      onClose={() => setIsOpen(false)}
+    >
+      <p>{t('registration.notEligible.text')}</p> {/* TODO: wrong text */}
+      <Icon className={styles.icon} src={personIcon} />
+    </AlertModal>
   );
 };
 
