2b6fc0c33eb6cde71b568f5682a8ce6fce0c0a93
Kari Salminen <kari.salminen@anders.com>
2023-03-29 12:22:08 +0300

    feat: show localized language name in parentheses in MoreInfoLinkList
    also:
     - remove title from link in MoreInfoLinkList
     - stylize the localized language name in parentheses to be less
       prominent as the native language name
     - update test snapshots
    
    Example:
     - Finnish: "Français (ranska)"
     - Swedish: "Français (Franska)"
     - English: "Français (French)"
    
    refs KK-1000

--- src/domain/home/moreInfo/MoreInfoLinkList.tsx
+++ src/domain/home/moreInfo/MoreInfoLinkList.tsx
@@ -2,25 +2,26 @@ import { useTranslation } from 'react-i18next';
 
 import styles from './moreInfoLinkList.module.scss';
 import { MoreInfoLink } from './types/moreInfo';
+import { getCurrentLanguage } from '../../../common/translation/TranslationUtils';
 
 type MoreInfoLinkListProps = {
   links: MoreInfoLink[];
 };
 
 const MoreInfoLinkList = ({ links }: MoreInfoLinkListProps) => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const currentLanguage = getCurrentLanguage(i18n);
   return (
     <div className={styles.link}>
       {links.map((link, index: number) => {
         const languageName = t(`home.moreInfo.links.${link.langCode}`);
         return (
-          <a
-            href={link.url}
-            lang={link.langCode}
-            key={index}
-            title={languageName}
-          >
-            {link.langName}
+          <a href={link.url} key={index}>
+            <span lang={link.langCode}>{link.langName}</span>
+            <span>&thinsp;</span>
+            <span lang={currentLanguage} className={styles.localizedLanguage}>
+              ({languageName})
+            </span>
           </a>
         );
       })}
