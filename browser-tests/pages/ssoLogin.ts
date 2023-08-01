e4ccb07517a789fc393cd6f75ad58ecb7809c46a
Tero Virtanen <tero.virtanen@mavericks.fi>
2023-08-01 12:19:23 +0300

    chore: browser test login language to English

--- browser-tests/pages/ssoLogin.ts
+++ browser-tests/pages/ssoLogin.ts
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/testcafe';
+import { Selector } from 'testcafe';
 
 export const ssoLogin = {
   loginLink: screen.getByText('Helsinki-tunnus'),
@@ -7,4 +8,6 @@ export const ssoLogin = {
   loginButton: screen.getByDisplayValue('Log In'),
   permissionPage: screen.queryByText('Älä anna lupaa'),
   givePermissionButton: screen.getByDisplayValue('Anna lupa'),
+  localeDropdown: Selector('#kc-locale-dropdown'),
+  localeLanguage: 'English',
 };
