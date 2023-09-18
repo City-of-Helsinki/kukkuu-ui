070c72468e80609b16a0ad454fb2d267a4681406
Niko Lindroos <niko.j.makela@gmail.com>
2023-09-18 12:53:05 +0300

    refactor: manage dependency resolution mismatches
    KK-1017.
    
    A temporary fix to replace `enzyme-adapter-react-17`
    with new unofficial `enzyme-adapter-react-18`.
    
    Immer needs to be under below v. 10, so that it would fit
    together with Redux.
    
    Updated deprecated functions usage of the JS Faker lib.
    
    Fixes to some dependency resolutions.

--- src/domain/app/state/AppStore.ts
+++ src/domain/app/state/AppStore.ts
@@ -2,10 +2,12 @@ import { configureStore, getDefaultMiddleware, Store } from '@reduxjs/toolkit';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { loadUser } from 'redux-oidc';
+import { enableES5 } from 'immer';
 
 import rootReducer from './AppReducers';
 import userManager from '../../auth/userManager';
-
+// Since Immer version 6, support for the fallback implementation has to be explicitly enabled by calling enableES5().
+enableES5();
 const persistConfig = {
   key: 'root',
   storage,
