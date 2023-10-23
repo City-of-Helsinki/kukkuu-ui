4bbf2b3fe6635c23d1a8b5686df71b1d7c141051
Ivan Melnik <melnik.job@gmail.com>
2023-10-23 18:17:44 +0300

    feat: cookie consent
    KK-569 KK-570.

--- src/domain/app/layout/PageLayout.tsx
+++ src/domain/app/layout/PageLayout.tsx
@@ -1,13 +1,18 @@
 import { HelmetProvider } from 'react-helmet-async';
+import { useTranslation } from 'react-i18next';
 
 import Navigation from '../navigation/Navigation';
 import Notification from '../notification/Notification';
 import Footer from '../footer/Footer';
 import styles from './pageLayout.module.scss';
+import { MAIN_CONTENT_ID } from '../../constants';
+import CookieConsent from '../../cookieConsent/CookieConsent';
 
 type PageLayoutProps = { children: React.ReactNode };
 
 function PageLayout({ children }: PageLayoutProps) {
+  const { t } = useTranslation();
+
   return (
     <HelmetProvider>
       <div className={styles.pageWrapper}>
@@ -16,7 +21,10 @@ function PageLayout({ children }: PageLayoutProps) {
           <Notification />
         </div>
 
-        <div className={styles.pageBody}>{children}</div>
+        <div className={styles.pageBody} id={MAIN_CONTENT_ID}>
+          {children}
+          <CookieConsent appName={t('appName')} />
+        </div>
 
         <Footer />
       </div>
