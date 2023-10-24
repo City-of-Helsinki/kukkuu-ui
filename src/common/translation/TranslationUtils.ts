2a46464873d0a64bdf660d5e2cb4e565b5560065
Niko Lindroos <niko.j.makela@gmail.com>
2023-10-24 17:36:37 +0300

    fix: language sync
    KK-1017

--- src/common/translation/TranslationUtils.ts
+++ src/common/translation/TranslationUtils.ts
@@ -8,6 +8,9 @@ type Language = 'fi' | 'en' | 'sv';
  * Safety check in case
  * somehow i18n have problem or have empty languages.
  */
-export const getCurrentLanguage = (i18n: I18nInstanceType): Language =>
-  (i18n && i18n.languages && (i18n.languages[0] as Language)) ||
-  SUPPORT_LANGUAGES.FI;
+export const getCurrentLanguage = (i18n: I18nInstanceType): Language => {
+  if (i18n.language && Object.values(i18n.languages).includes(i18n.language)) {
+    return i18n.language as Language;
+  }
+  return SUPPORT_LANGUAGES.FI as Language;
+};
