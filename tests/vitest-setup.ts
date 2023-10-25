872f463bd5368828a972f10b3343ce6edf0b190c
Niko Lindroos <niko.j.makela@gmail.com>
2023-10-25 14:52:55 +0300

    test: mock resize observer and update layout snapshot
    KK-1040

--- tests/vitest-setup.ts
+++ tests/vitest-setup.ts
@@ -1,13 +1,19 @@
 /* eslint-disable import/first */
 // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
 require('dotenv').config({ path: './.env.test' });
-import { beforeAll, afterEach, afterAll } from 'vitest';
+import { beforeAll, afterEach, afterAll, vi } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/vitest';
 
 import setLocale from '../src/common/localization/setLocale';
 import { server } from '../src/test/msw/server';
 import '../src/common/test/testi18nInit';
+
+global.ResizeObserver = vi.fn().mockImplementation(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn(),
+}));
 
 afterEach(() => {
   cleanup();
