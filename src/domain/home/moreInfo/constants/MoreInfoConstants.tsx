f429f1786b66f4921b3dfc93e7b6a5af310d66f7
Niko Lindroos <niko.j.makela@gmail.com>
2023-12-08 13:55:13 +0200

    fix: front page info links to brochure pdf's
    KK-1060

--- src/domain/home/moreInfo/constants/MoreInfoConstants.tsx
+++ src/domain/home/moreInfo/constants/MoreInfoConstants.tsx
@@ -4,80 +4,80 @@ export const moreInfoLinks: MoreInfoLink[] = [
   {
     langName: 'Français',
     langCode: 'fr',
-    url: `${import.meta.env.PUBLIC_URL}/brochures/fr.pdf`,
+    url: '/brochures/fr.pdf',
   },
   {
     langName: 'Русский',
     langCode: 'ru',
-    url: `${import.meta.env.PUBLIC_URL}/brochures/ru.pdf`,
+    url: '/brochures/ru.pdf',
   },
   {
     langName: 'Español',
     langCode: 'es',
-    url: `${import.meta.env.PUBLIC_URL}/brochures/es.pdf`,
+    url: '/brochures/es.pdf',
   },
 
   {
     langName: '中文',
     langCode: 'zh',
-    url: `${import.meta.env.PUBLIC_URL}/brochures/zh.pdf`,
+    url: '/brochures/zh.pdf',
   },
   {
     langName: 'नेपाली गण',
     langCode: 'npi',
-    url: `${import.meta.env.PUBLIC_URL}/brochures/npi.pdf`,
+    url: '/brochures/npi.pdf',
   },
   {
     langName: 'Somali',
     langCode: 'som',
-    url: `${import.meta.env.PUBLIC_URL}/brochures/som.pdf`,
+    url: '/brochures/som.pdf',
   },
 
   {
     langName: 'زبان فارسی',
     langCode: 'fas',
-    url: `${import.meta.env.PUBLIC_URL}/brochures/fas.pdf`,
+    url: '/brochures/fas.pdf',
   },
   {
     langName: 'Eesti',
     langCode: 'et',
-    url: `${import.meta.env.PUBLIC_URL}/brochures/et.pdf`,
+    url: '/brochures/et.pdf',
   },
   {
     langName: 'Türkçe',
     langCode: 'tur',
-    url: `${import.meta.env.PUBLIC_URL}/brochures/tur.pdf`,
+    url: '/brochures/tur.pdf',
   },
 
   {
     langName: 'اَلْعَرَبِيَّةُ',
     langCode: 'ar',
-    url: `${import.meta.env.PUBLIC_URL}/brochures/ara.pdf`,
+    url: '/brochures/ara.pdf',
   },
   {
     langName: 'کوردی',
     langCode: 'ckb',
-    url: `${import.meta.env.PUBLIC_URL}/brochures/ckb.pdf`,
+    url: '/brochures/ckb.pdf',
   },
   {
     langName: 'Tiếng Việt',
     langCode: 'vi',
-    url: `${import.meta.env.PUBLIC_URL}/brochures/vi.pdf`,
+    url: '/brochures/vi.pdf',
   },
 
   {
     langName: 'English',
     langCode: 'en',
-    url: `${import.meta.env.PUBLIC_URL}/brochures/en.pdf`,
+    url: '/brochures/en.pdf',
   },
   {
     langName: 'Svenska',
     langCode: 'sv',
-    url: `${import.meta.env.PUBLIC_URL}/brochures/sv.pdf`,
+    url: '/brochures/sv.pdf',
   },
   {
     langName: 'Suomi',
     langCode: 'fi',
-    url: `${import.meta.env.PUBLIC_URL}/brochures/fi.pdf`,
+    url: '/brochures/fi.pdf',
   },
 ];
