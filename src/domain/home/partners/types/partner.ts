92c956bdb1dbaa6c47fb84eb6994be1e868f2627
Kari Salminen <kari.salminen@anders.com>
2024-01-14 13:56:11 +0200

    fix: use official SVG JAES logos from jaes.fi

--- src/domain/home/partners/types/partner.ts
+++ src/domain/home/partners/types/partner.ts
@@ -1,6 +1,7 @@
 export interface Partner {
   name: string;
   icon: string;
+  iconClassName?: string;
   altLangIcons?: { [key: string]: string }; // Icons in other languages
   url: {
     fi: string;
