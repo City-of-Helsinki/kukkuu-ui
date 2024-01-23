d821e3435ff16c231a2c00ab31aa99539d030dc5
Kari Salminen <kari.salminen@anders.com>
2024-01-23 15:50:15 +0200

    fix: running generate:graphql, replace REACT_APP_API_URI -> VITE_API_URI

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  schema: process.env.REACT_APP_API_URI,
+  schema: process.env.VITE_API_URI,
   documents: ['src/**/*.ts'],
   generates: {
     'src/domain/api/generatedTypes/graphql.tsx': {
