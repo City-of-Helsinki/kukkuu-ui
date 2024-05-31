b5df22fdf451724ddec26f7975aa857cf5f803d8
Kari Salminen <kari.salminen@anders.com>
2024-05-31 11:43:09 +0300

    fix: disable editing email field in registration form
    refs KK-1174

--- src/domain/registration/form/__tests__/RegistrationForm.test.tsx
+++ src/domain/registration/form/__tests__/RegistrationForm.test.tsx
@@ -1,4 +1,7 @@
-import RegistrationForm, { FORM_TESTID } from '../RegistrationForm';
+import RegistrationForm, {
+  EMAIL_FIELD_TESTID,
+  FORM_TESTID,
+} from '../RegistrationForm';
 import { render, screen } from '../../../../common/test/testingLibraryUtils';
 
 // TODO: Needs mocks for profile query and redux selector.
@@ -6,4 +9,10 @@ it.skip('renders snapshot correctly', async () => {
   const { container } = render(<RegistrationForm />);
   expect(await screen.findByTestId(FORM_TESTID));
   expect(container).toMatchSnapshot();
+});
+
+it('email field is disabled', async () => {
+  render(<RegistrationForm />);
+  const emailField = await screen.findByTestId(EMAIL_FIELD_TESTID);
+  expect(emailField.hasAttribute('disabled'));
 });
