ed32b71363d3ba76241ceb523ac5f3b0fc4ef1a4
Ivan Melnik <melnik.job@gmail.com>
2024-06-10 12:37:10 +0300

    fix: removed unused code

--- src/domain/termsOfService/__tests__/TermsOfService.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from '../../../common/test/testingLibraryUtils';
-import TermsOfService from '../TermsOfService';
-
-it('renders snapshot correctly', () => {
-  const { container } = render(<TermsOfService />);
-  expect(container).toMatchSnapshot();
-});
