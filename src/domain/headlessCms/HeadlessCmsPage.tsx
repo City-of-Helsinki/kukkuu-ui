10f1d0fd93d508b3dda52bfde1cb61caba4ea445
Niko Lindroos <niko.j.makela@gmail.com>
2024-09-30 17:35:21 +0300

    fix: page meta of the CMS pages
    KK-824.
    
    The CMS page's meta tags are now extended to the Kukkuu PageMeta's content.
    
    NOTE: This means that SEO information needs to be written in the CMS side.

--- src/domain/headlessCms/HeadlessCmsPage.tsx
+++ src/domain/headlessCms/HeadlessCmsPage.tsx
@@ -3,27 +3,23 @@ import { useLocation } from 'react-router-dom';
 
 import styles from './headlessCmsPage.module.scss';
 import NotFound from '../app/notFound/NotFound';
-import PageMeta from '../app/layout/utilityComponents/PageMeta';
 
 const HeadlessCmsPage = () => {
   const location = useLocation();
 
   return (
-    <>
-      <PageMeta title={'appName'} />
-      <Page
-        uri={location.pathname}
-        className={styles.cmsPageContainer}
-        navigation={undefined}
-        content={
-          <PageContent
-            notFoundPageContent={<NotFound />}
-            collections={() => null}
-          />
-        }
-        footer={undefined}
-      />
-    </>
+    <Page
+      uri={location.pathname}
+      className={styles.cmsPageContainer}
+      navigation={undefined}
+      content={
+        <PageContent
+          notFoundPageContent={<NotFound />}
+          collections={() => null}
+        />
+      }
+      footer={undefined}
+    />
   );
 };
 
