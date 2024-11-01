ff722151963d35b361d3c3372875bdb5a89b36fd
Ivan Melnik <melnik.job@gmail.com>
2024-11-01 11:24:30 +0200

    fix: enable sentry in all envs

--- src/index.tsx
+++ src/index.tsx
@@ -12,16 +12,14 @@ import AppConfig from './domain/app/AppConfig';
 initI18next();
 Modal.setAppElement('#root');
 
-if (import.meta.env.NODE_ENV === 'production') {
-  Sentry.init({
-    dsn: import.meta.env.VITE_SENTRY_DSN,
-    environment: import.meta.env.VITE_ENVIRONMENT,
-    release: `${import.meta.env.VITE_APPLICATION_NAME}@${
-      import.meta.env.VITE_VERSION
-    }`,
-    autoSessionTracking: false,
-  });
-}
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  environment: import.meta.env.VITE_ENVIRONMENT,
+  release: `${import.meta.env.VITE_APPLICATION_NAME}@${
+    import.meta.env.VITE_VERSION
+  }`,
+  autoSessionTracking: false,
+});
 const container = document.getElementById('root') as Element;
 const root = createRoot(container);
 
