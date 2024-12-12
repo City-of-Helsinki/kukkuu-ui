adc875a77af6c36bf5309f94a1fcea927896e0c9
Kari Salminen <kari.salminen@anders.com>
2024-12-12 14:20:07 +0200

    fix: warnings in ProfileEvents.test.tsx by fixing mocks
    also:
     - enable full apollo client error messages for development to ease
       debugging, see https://www.apollographql.com/docs/react/errors
    
    From https://www.apollographql.com/docs/react/errors
    > Beginning with version 3.8, Apollo Client omits error messages from
    > its core bundle to reduce its bundle size. Instead, errors direct you
    > to this page to view details.
    >
    > If you prefer, you can add Apollo Client error messages to your app:
    >
    > loadDevMessages() loads messages for errors that are raised only in a
    > development environment.
    >
    > loadErrorMessages() loads messages for errors that are raised in all
    > environments. You can call either or both of these methods on
    > application load.
    
    refs KK-1372

--- tests/vitest-setup.ts
+++ tests/vitest-setup.ts
@@ -3,10 +3,15 @@
 require('dotenv').config({ path: './.env.test' });
 import { beforeAll, afterEach, afterAll, vi } from 'vitest';
 import '@testing-library/jest-dom/vitest';
+import { loadErrorMessages, loadDevMessages } from '@apollo/client/dev';
 
 import setLocale from '../src/common/localization/setLocale';
 import { server } from '../src/test/msw/server';
 import '../src/common/test/testi18nInit';
+
+// Load error messages for Apollo client so it's easier to debug errors
+loadDevMessages();
+loadErrorMessages();
 
 global.ResizeObserver = vi.fn().mockImplementation(() => ({
   observe: vi.fn(),
