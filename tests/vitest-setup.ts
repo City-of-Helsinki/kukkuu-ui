f3b0df6c6fa38058150526af306b67498d753cbf
Kari Salminen <kari.salminen@anders.com>
2024-12-13 16:00:59 +0200

    fix: warning in AddNewChildFormModal.test.tsx by using Modal appElement
    refs KK-1372

--- tests/vitest-setup.ts
+++ tests/vitest-setup.ts
@@ -1,6 +1,8 @@
 /* eslint-disable import/first */
 // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
 require('dotenv').config({ path: './.env.test' });
+
+import Modal from 'react-modal';
 import { beforeAll, afterEach, afterAll, vi } from 'vitest';
 import '@testing-library/jest-dom/vitest';
 import { loadErrorMessages, loadDevMessages } from '@apollo/client/dev';
@@ -12,6 +14,15 @@ import '../src/common/test/testi18nInit';
 // Load error messages for Apollo client so it's easier to debug errors
 loadDevMessages();
 loadErrorMessages();
+
+// Set an appElement for react-modal for tests. For production it is set in
+// src/index.tsx. Otherwise react-modal raises warning:
+//
+// "Warning: react-modal: App element is not defined. Please use
+// `Modal.setAppElement(el)` or set `appElement={el}`. This is needed so screen
+// readers don't see main content when modal is opened. It is not recommended,
+// but you can opt-out by setting `ariaHideApp={false}`."
+Modal.setAppElement(document.createElement('div'));
 
 global.ResizeObserver = vi.fn().mockImplementation(() => ({
   observe: vi.fn(),
