389ada2296bd3e0229f640c1499396e859e02353
Kari Salminen <kari.salminen@anders.com>
2024-12-16 13:49:53 +0200

    fix: warnings in EventIsEnrolled.test.tsx by matching mocks
    refs KK-1372

--- src/domain/event/__tests__/EventIsEnrolled.test.tsx
+++ src/domain/event/__tests__/EventIsEnrolled.test.tsx
@@ -1,7 +1,24 @@
+import { MockedResponse } from '@apollo/client/testing';
+
 import { render } from '../../../common/test/testingLibraryUtils';
 import EventIsEnrolled from '../EventIsEnrolled';
+import occurrenceQuery from '../queries/occurrenceQuery';
+
+const emptyOccurrenceMock: MockedResponse = {
+  request: {
+    query: occurrenceQuery,
+    variables: { id: undefined, childId: undefined },
+  },
+  result: {
+    data: {
+      occurrence: {},
+    },
+  },
+};
+
+const mocks: MockedResponse[] = [emptyOccurrenceMock];
 
 it('renders snapshot correctly', () => {
-  const { container } = render(<EventIsEnrolled />);
+  const { container } = render(<EventIsEnrolled />, mocks);
   expect(container).toMatchSnapshot();
 });
