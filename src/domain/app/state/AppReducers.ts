df5e5c2b7925d6bf6fe20df360c19bd18a65272a
Kari Salminen <kari.salminen@anders.com>
2024-12-19 11:14:27 +0200

    fix: deprecation warning, change react-transition-group → react-toastify
    remove deprecation warning by switching to react-toastify:
    ```
    Warning: findDOMNode is deprecated and will be removed in the next major
    release. Instead, add a ref directly to the element you want to
    reference. Learn more about using refs safely here:
    https://reactjs.org/link/strict-mode-find-node
        at Transition (node_modules\react-transition-group\cjs\Transition...
        at CSSTransition (node_modules\react-transition-group\cjs\CSSTran...
        at SuccessToast (src\domain\event\enrol\SuccessToast.tsx:24:41)
    ```
    
    also:
     - remove unused leftover eslintConfig setting from package.json
     - remove react-transition-group & its types package
     - update react-toastify to latest version
     - remove justEnrolled action related to Redux as unneeded
     - remove SuccessToast component and its test as it's toast.success now
     - make all toast messages not autoclose for accessibility
    
    refs KK-1372

--- src/domain/app/state/AppReducers.ts
+++ src/domain/app/state/AppReducers.ts
@@ -3,10 +3,8 @@ import { combineReducers } from '@reduxjs/toolkit';
 import profileReducer from '../../profile/state/ProfileReducers';
 import registrationReducer from '../../registration/state/RegistrationReducers';
 import EventReducers from '../../event/state/EventReducers';
-import EnrolledReducers from '../../event/state/EnrolledReducers';
 
 export default combineReducers({
-  enrolled: EnrolledReducers,
   event: EventReducers,
   registration: registrationReducer,
   profile: profileReducer,
