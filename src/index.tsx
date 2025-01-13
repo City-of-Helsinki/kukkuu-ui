5e17e177e1346ee15d58dffa7c2267bf438ab988
Niko Lindroos <niko.j.makela@gmail.com>
2025-01-13 13:17:59 +0200

    chore: ignore permission denied in Sentry
    KK-1398

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import * as Sentry from '@sentry/browser';
 import './assets/styles/main.scss';
 import 'hds-core/lib/base.css';
 import Modal from 'react-modal';
@@ -8,19 +7,13 @@ import React from 'react';
 import BrowserApp from './domain/app/BrowserApp';
 import { initI18next } from './common/translation/i18n/i18nInit';
 import AppConfig from './domain/app/AppConfig';
+import { initSentry } from './utils/sentry';
 
 initI18next();
 Modal.setAppElement('#root');
 
 if (AppConfig.isAppInProductionMode) {
-  Sentry.init({
-    dsn: import.meta.env.VITE_SENTRY_DSN,
-    environment: AppConfig.environment,
-    release: `${import.meta.env.VITE_APPLICATION_NAME}@${
-      import.meta.env.VITE_VERSION
-    }`,
-    autoSessionTracking: false,
-  });
+  initSentry();
 }
 const container = document.getElementById('root') as Element;
 const root = createRoot(container);
