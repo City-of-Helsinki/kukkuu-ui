c1569f5e1749a0a29e0849e44785966cc01db6cb
Niko Lindroos <niko.j.makela@gmail.com>
2025-02-19 12:12:16 +0200

    feat: use apollo client local storage cache persister
    KK-1406.
    
    Installed apollo3-cache-persist.
    
    Refactored code by spitting the CMS client code to multiple files.
    
    Implemented `TimedApolloCachePersistor` that creates a new persisted
    Apollo cache persistor with an expiration time. This class automatically
    clears expired caches and provides methods for persisting and restoring
    the cache to/from localStorage.
    
    The language(s) queries are now hard coded to the Apollo client's in
    memory cache in the apollo client's initialization, so there is no need
    to handle cache or queries related those anymore.

--- src/domain/headlessCms/__tests__/client.test.ts
+++ src/domain/headlessCms/__tests__/client.test.ts
@@ -1,13 +1,21 @@
 import { graphql, HttpResponse } from 'msw';
 import { PageDocument } from 'react-helsinki-headless-cms/apollo';
+import {
+  ApolloClient,
+  InMemoryCache,
+  NormalizedCacheObject,
+} from '@apollo/client';
 
 import { server } from '../../../test/msw/server';
 import { fakePage } from '../../../utils/cmsMockDataUtils';
-import client from '../client';
+import { createCmsApolloClient } from '../client';
+import { LogLevel, TimedApolloCachePersistor } from '../persistor';
 
+let client: ApolloClient<NormalizedCacheObject>;
 const page = fakePage();
 
 beforeEach(() => {
+  client = createCmsApolloClient();
   const link =
     process.env.VITE_CMS_URI ||
     'https://kukkuu.app-staging.hkih.hion.dev/graphql';
@@ -22,7 +30,7 @@ describe('Headless CMS Client', () => {
     const { data } = await client.query({
       query: PageDocument,
       variables: {
-        id: '/en/slug',
+        id: page!.id,
         language: 'EN',
       },
       // NOTE: The React-helsinki-headless-cms library sets a cache here by default
@@ -30,5 +38,172 @@ describe('Headless CMS Client', () => {
     });
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     expect(data.page.id).toEqual(page!.id);
+  });
+
+  it('returns a page from the cache when a page query is requested', async () => {
+    const variables = {
+      id: page!.id,
+      language: 'EN',
+    };
+    // First query to populate the cache
+    await client.query({
+      query: PageDocument,
+      variables,
+      fetchPolicy: 'cache-first',
+    });
+
+    // Second query to test cache retrieval
+    const { data } = await client.query({
+      query: PageDocument,
+      variables,
+      fetchPolicy: 'cache-only',
+    });
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    expect(data.page.id).toEqual(page!.id);
+  });
+
+  describe('persisted cache', () => {
+    afterEach(() => {
+      localStorage.clear();
+      vi.restoreAllMocks();
+      vi.useRealTimers();
+      client.cache.reset(); // Reset the cache after each test.
+    });
+
+    it('clears the local storage on initialization when the persisted cache has expired', async () => {
+      const hasPersistedCacheExpiredSpy = vi
+        .spyOn(TimedApolloCachePersistor.prototype, 'hasPersistedCacheExpired')
+        .mockImplementation(() => true);
+      const clearLocalStorageSpy = vi.spyOn(
+        TimedApolloCachePersistor.prototype,
+        'clearLocalStorage'
+      );
+      const cachePersistor = new TimedApolloCachePersistor(
+        client.cache as InMemoryCache,
+        {
+          logLevel: LogLevel.WARN,
+          persistedCacheTimeToLiveMs: 0, // Set TTL to 0 to simulate expiration
+        }
+      );
+      expect(cachePersistor.hasPersistedCacheExpired()).toBe(true);
+      expect(hasPersistedCacheExpiredSpy).toHaveBeenCalledTimes(2);
+      expect(clearLocalStorageSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('does not clear the local storage when the persisted cache has not expired', async () => {
+      const hasPersistedCacheExpiredSpy = vi
+        .spyOn(TimedApolloCachePersistor.prototype, 'hasPersistedCacheExpired')
+        .mockReturnValue(false);
+      const clearLocalStorageSpy = vi.spyOn(
+        TimedApolloCachePersistor.prototype,
+        'clearLocalStorage'
+      );
+      const cachePersistor = new TimedApolloCachePersistor(
+        client.cache as InMemoryCache,
+        {
+          logLevel: LogLevel.WARN,
+          persistedCacheTimeToLiveMs: 10000,
+        }
+      );
+      expect(cachePersistor.hasPersistedCacheExpired()).toBe(false);
+      expect(hasPersistedCacheExpiredSpy).toHaveBeenCalledTimes(2);
+      expect(clearLocalStorageSpy).not.toHaveBeenCalled();
+    });
+
+    it('persists the cache to localStorage', async () => {
+      const cachePersistor = new TimedApolloCachePersistor(
+        client.cache as InMemoryCache,
+        {
+          logLevel: LogLevel.WARN,
+          persistedCacheTimeToLiveMs: 10000,
+        }
+      );
+
+      await cachePersistor.persist();
+      const persistedData = localStorage.getItem(
+        cachePersistor.persistorLocalStorageKey
+      );
+      expect(persistedData).not.toBeNull();
+      const persistedTimestamp = localStorage.getItem(
+        cachePersistor.timePersistedLocalStorageKey
+      );
+      expect(persistedTimestamp).not.toBeNull();
+    });
+
+    it('restores the cache from localStorage', async () => {
+      // Make a query to write cache
+      await client.query({
+        query: PageDocument,
+        variables: {
+          id: page!.id,
+          language: 'EN',
+        },
+        fetchPolicy: 'network-only',
+      });
+
+      // Create a persistor
+      const cachePersistor = new TimedApolloCachePersistor(
+        client.cache as InMemoryCache,
+        {
+          logLevel: LogLevel.WARN,
+          persistedCacheTimeToLiveMs: 10000,
+        }
+      );
+
+      // Persist to localStorage
+      cachePersistor.persist();
+
+      // reset the client cache
+      client.cache.reset();
+
+      // Test that the cache is empty
+      const { data: dataBeforeRestore } = await client.query({
+        query: PageDocument,
+        variables: {
+          id: page!.id,
+          language: 'EN',
+        },
+        fetchPolicy: 'cache-only',
+      });
+      expect(dataBeforeRestore).toEqual({});
+
+      // Restore the cache from locaStorage
+      await cachePersistor.restore();
+
+      // Test that the cache is restored
+      const { data: dataAfterRestore } = await client.query({
+        query: PageDocument,
+        variables: {
+          id: page!.id,
+          language: 'EN',
+        },
+        fetchPolicy: 'cache-only',
+      });
+      expect(dataAfterRestore.page.id).toEqual(page!.id);
+    });
+
+    it.each([600000, 10000])(
+      'expires the persisted cache in set time (%i ms)',
+      async (persistedCacheTimeToLiveMs) => {
+        vi.useFakeTimers();
+        const cachePersistor = new TimedApolloCachePersistor(
+          client.cache as InMemoryCache,
+          {
+            persistedCacheTimeToLiveMs,
+          }
+        );
+
+        const persistedAt = Date.now();
+        localStorage.setItem(
+          cachePersistor.timePersistedLocalStorageKey,
+          persistedAt.toString()
+        );
+
+        expect(cachePersistor.hasPersistedCacheExpired()).toBe(false);
+
+        vi.advanceTimersByTime(persistedCacheTimeToLiveMs + 1);
+        expect(cachePersistor.hasPersistedCacheExpired()).toBe(true);
+      }
+    );
   });
 });
--- src/domain/headlessCms/client.ts
+++ src/domain/headlessCms/client.ts
@@ -1,52 +1,103 @@
-import {
-  ApolloClient,
-  createHttpLink,
-  InMemoryCache,
-  ApolloLink,
-} from '@apollo/client';
+import { ApolloClient, createHttpLink, ApolloLink } from '@apollo/client';
 import { onError } from '@apollo/client/link/error';
 import * as Sentry from '@sentry/browser';
-import { LanguagesDocument } from 'react-helsinki-headless-cms/apollo';
 
 import AppConfig from '../app/AppConfig';
-import { HARDCODED_CMS_LANGUAGE_QUERY_RESPONSE } from '../languages/constants';
+import {
+  createApolloCache,
+  isConfiguredQueryPolicy,
+  queryPolicies,
+} from './cache';
+import { LogLevel, TimedApolloCachePersistor } from './persistor';
 
-const httpLink = createHttpLink({
-  uri: import.meta.env.VITE_CMS_URI,
-});
+export function createCmsApolloClient() {
+  const httpLink = createHttpLink({
+    uri: import.meta.env.VITE_CMS_URI,
+  });
 
-const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors) {
-    graphQLErrors.forEach(({ message, locations, path, extensions }) => {
-      const errorCode = extensions?.code;
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors) {
+      graphQLErrors.forEach(({ message, locations, path, extensions }) => {
+        const errorCode = extensions?.code;
 
-      // eslint-disable-next-line max-len
-      const errorMessage = `[GraphQL error]: Message: ${message}, Code: ${errorCode}, Location: ${locations}, Path: ${path}`;
+        // eslint-disable-next-line max-len
+        const errorMessage = `[GraphQL error]: Message: ${message}, Code: ${errorCode}, Location: ${locations}, Path: ${path}`;
 
-      Sentry.captureMessage(errorMessage);
+        Sentry.captureMessage(errorMessage);
 
-      if (!AppConfig.isAppInProductionMode) {
-        // eslint-disable-next-line no-console
-        console.error(errorMessage);
-      }
-    });
-  }
+        if (!AppConfig.isAppInProductionMode) {
+          // eslint-disable-next-line no-console
+          console.error(errorMessage);
+        }
+      });
+    }
 
-  if (networkError) {
-    Sentry.captureMessage('Network error');
-  }
-});
+    if (networkError) {
+      Sentry.captureMessage('Network error');
+    }
+  });
 
-const client = new ApolloClient({
-  link: ApolloLink.from([errorLink, httpLink]),
-  cache: new InMemoryCache(),
-});
+  const cache = createApolloCache();
+  const kukkuuApolloCachePersistor = new TimedApolloCachePersistor(cache, {
+    logLevel: LogLevel.WARN,
+    persistedCacheTimeToLiveMs: AppConfig.apolloPersistedCacheTimeToLiveMs,
+  });
 
-// Make sure the hardcoded CMS languages response is always in cache
-client.writeQuery({
-  query: LanguagesDocument,
-  data: { ...HARDCODED_CMS_LANGUAGE_QUERY_RESPONSE },
-  variables: {},
-});
+  (async () => {
+    if (kukkuuApolloCachePersistor.hasPersistedCacheExpired()) {
+      // eslint-disable-next-line no-console
+      console.info('Persisted cache has expired.');
+      await kukkuuApolloCachePersistor.purge();
+    } else {
+      await kukkuuApolloCachePersistor.restore();
+      // eslint-disable-next-line no-console
+      console.info('Persisted cache has been restored.');
+    }
+  })();
+
+  const client = new ApolloClient({
+    link: ApolloLink.from([errorLink, httpLink]),
+    cache,
+    defaultOptions: {
+      watchQuery: {
+        nextFetchPolicy(
+          currentFetchPolicy,
+          {
+            // Either "after-fetch" or "variables-changed", indicating why the
+            // nextFetchPolicy function was invoked.
+            reason,
+            // The rest of the options (currentFetchPolicy === options.fetchPolicy).
+            // The original value of options.fetchPolicy, before nextFetchPolicy was
+            // applied for the first time.
+            initialFetchPolicy,
+            // The ObservableQuery associated with this client.watchQuery call.
+            observable,
+          }
+        ) {
+          // When variables change, the default behavior is to reset
+          // options.fetchPolicy to context.initialFetchPolicy. If you omit this logic,
+          // your nextFetchPolicy function can override this default behavior to
+          // prevent options.fetchPolicy from changing in this case.
+          if (reason === 'variables-changed') {
+            return initialFetchPolicy;
+          }
+          // The languages are hardcoded in the cache, so we can always return cache-only,
+          // because the languages never changes. Also, flashing on menus should be avoided,
+          // so the menu query should first try from cache, but could still update cache
+          // on background.
+          if (isConfiguredQueryPolicy(observable.queryName)) {
+            return queryPolicies[observable.queryName];
+          }
+          // Leave all other fetch policies unchanged.
+          return currentFetchPolicy;
+        },
+      },
+    },
+  });
+
+  return client;
+}
+
+const client = createCmsApolloClient();
 
 export default client;
