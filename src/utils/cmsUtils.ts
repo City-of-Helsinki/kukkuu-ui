143851c7daff51ba7916d5e0c097590fdcb167f3
Kari Salminen <kari.salminen@anders.com>
2025-02-20 06:31:10 +0200

    fix(sonarcloud): fix removeSurroundingSlashes regex & test it works
    fix SonarCloud code bug typescript:S5850:
    > Group parts of the regex together to make the intended operator
    > precedence explicit.
    > Alternatives in regular expressions should be grouped when used with
    > anchors typescript:S5850
    
    See rule: https://rules.sonarsource.com/typescript/RSPEC-5850/
    
    refs KK-1416

--- src/utils/cmsUtils.ts
+++ src/utils/cmsUtils.ts
@@ -2,8 +2,14 @@ export const stripLocaleFromUri = (uri: string): string => {
   return uri.replace(/^\/(en|sv|fi)(?![a-z0-9])/i, '');
 };
 
+/**
+ * Remove a single slash from given path's beginning and from its end.
+ * @param path
+ * @returns Input path with single slash, if present, removed from its beginning
+ * and similarly from its end.
+ * */
 export const removeSurroundingSlashes = (path: string) => {
-  return path.replace(/^\/|\/$/g, '');
+  return path.replace(/(^\/|\/$)/g, '');
 };
 
 export const normalizeCmsUri = (uri: string) => {
